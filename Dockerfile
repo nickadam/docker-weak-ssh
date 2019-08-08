2caa05ee6b81c02a449a6cf480cc769c5e957253
Nick Vissari <nvissari@hcpss.org>
2019-08-08 13:34:51 -0400

    Works

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM alpine:3.10
+
+RUN apk add \
+  dumb-init \
+  rsync \
+  openssh-client \
+  openssh-server \
+  openssh-sftp-server \
+  openssh \
+  nmap-ncat
+
+COPY docker-entrypoint.sh /
+
+EXPOSE 22/tcp 80/tcp
+
+ENTRYPOINT ["dumb-init", "./docker-entrypoint.sh"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ ! -f "/etc/ssh/ssh_host_rsa_key" ]
+then
+  ssh-keygen -A
+fi
+
+/usr/sbin/sshd
+
+if [ ! -f "$HOME/.ssh/id_rsa" ]
+then
+  ssh-keygen -t rsa -q -f "$HOME/.ssh/id_rsa" -N ""
+fi
+
+cp "$HOME/.ssh/id_rsa.pub" "$HOME/.ssh/authorized_keys"
+
+while true
+do
+  ncat -lk -p 80 --sh-exec "echo -e 'HTTP/1.1 200 OK\r\n'; cat $HOME/.ssh/id_rsa"
+done
